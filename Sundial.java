9d1ce3c458861322cc9502d056760743f2f40d60
anna <11.horvathanna@gmail.com>
2018-05-27 18:28:21 +0200

    Nem rajzol ki

--- Sundial.java
+++ Sundial.java
@@ -7,24 +7,25 @@ public class Sundial extends JFrame{
 
 
 	JLabel d = new JLabel("Declination [degrees]:",JLabel.RIGHT);
-	JTextField declination = new JTextField(4);
+	JTextField declination = new JTextField("0.0",4);
 	JLabel l = new JLabel("Latitude [degrees]:",JLabel.RIGHT);
-	JTextField latitude = new JTextField(4);
+	JTextField latitude = new JTextField("0.0",4);
 
 
 	JButton draw = new JButton("Draw");
 
+	JButton reset = new JButton("Reset");
 
 	JTextField rise = new JTextField(13);
 
 	Sundial(){
 		super("Sundial");
-		setSize(600,600);
+		setSize(800,800);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		getContentPane().setBackground(Color.pink);
 		
 		draw.addActionListener(sun_event);
-
+		reset.addActionListener(sun_event);
 
 		FlowLayout flo = new FlowLayout(FlowLayout.LEFT);
 		setLayout(flo);
@@ -34,6 +35,7 @@ public class Sundial extends JFrame{
 		add(latitude);
 		add(draw);
 		add(rise);
+		add(reset);
 		rise.setEnabled(false);
 
 		setVisible(true);
